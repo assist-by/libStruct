3616b11594b6971354281a04ef651e5210ac7cef
박기현 <kihyun1998@penta.co.kr>
2024-08-15 10:49:08 +0900

    Create technical_indicators.go

---
+++ technical_indicators.go
@@ -0,0 +1,10 @@
+package autrolibrary
+
+type TechnicalIndicators struct {
+	EMA200         float64
+	ParabolicSAR   float64
+	MACDLine       float64
+	SignalLine     float64
+	PrevMACDLine   float64
+	PrevSignalLine float64
+}
