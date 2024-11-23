273483e821c2628289cdbb74ac64a52f60656a6d
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2024-11-23 19:56:40 +0900

    Update: signal result에 signal type 추가

--- signal_result.go
+++ signal_result.go
@@ -2,10 +2,22 @@ package libStruct
 
 type SignalResult struct {
 	Symbol     string
-	Signal     string
+	Signal     SignalType
 	Timestamp  int64
 	Price      float64
 	StopLoss   float64
 	TakeProfit float64
 	Conditions SignalConditions
+}
+
+func NewSignalResult(symbol string, signal SignalType, timestamp int64, price, stoploss, takeProfit float64, conditions SignalConditions) (*SignalResult, error) {
+	return &SignalResult{
+		Symbol:     symbol,
+		Signal:     signal,
+		Timestamp:  timestamp,
+		Price:      price,
+		StopLoss:   stoploss,
+		TakeProfit: takeProfit,
+		Conditions: conditions,
+	}, nil
 }
